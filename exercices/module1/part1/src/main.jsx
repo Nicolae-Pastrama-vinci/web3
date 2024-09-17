6d5802b0b6edd7972e248ed8f4ab7043115cbd3e
nicolae.pastrama <nicolae.pastrama@student.vinci.be>
2024-09-17 16:42:30 +0200

    1.1-1.2 : refactor - absolute imports

--- exercices/module1/part1/src/main.jsx
+++ exercices/module1/part1/src/main.jsx
@@ -1,5 +1,4 @@
+import App from 'components/App/App'
 import ReactDOM from 'react-dom/client'
-
-import App from './components/App/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
