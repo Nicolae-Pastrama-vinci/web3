1f031e29f957d57dd0b99795e035ff49d4a512d5
Nicolae-Pastrama-vinci <nicolae.pastrama@student.vinci.be>
2024-09-24 15:39:46 +0200

    new:ex2.1-stored

--- exercices/module2/part1/src/Components/ClickCounter/ClickCounter.jsx
+++ exercices/module2/part1/src/Components/ClickCounter/ClickCounter.jsx
@@ -1,23 +1,27 @@
 import { useState } from 'react'
 
 const ClickCounter = ({title, messageClicks, message }) => {
-    const handleClick = () =>{
-      setCount((count) => count + 1);
-    }
-    
-    const handleEnter = () =>
-     {
-        setEnter(true);
-    }
-    const handleLeave = () =>{
-      setEnter(false);
-    }
-    const [count, setCount] = useState(0);
+    const db = JSON.parse(localStorage.getItem("count"));
+    const [count, setCount] = useState(db);
     const [enter, setEnter] = useState(false);
+    
       return(
         <>
           <h1>{title}</h1>
-          <button onClick={handleClick} onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+          <button 
+            onClick={() => {
+                setCount((count) => count + 1);
+                localStorage.setItem("count", JSON.stringify(count)); 
+              }
+            } 
+            onMouseEnter={()=> {
+                setEnter(true)
+              }
+            } 
+            onMouseLeave={() => {
+                setEnter(false)
+              }
+            }>
               {enter ? <p>{message}</p> : null}
               count is {count}
           </button>
